- Few fixes to run benchmark on SUMMIT - Add script for generating job scripts for scaling experiments starting from a template

src/training/benchmark.py
@@ -243,6 +243,7 @@ def main():
 
     if args.synthetic_data:
         dataloader = [0 for i in range(args.bench_steps)]
+        sampler = None
     else:
         dataloader, sampler = data['train'].dataloader, data['train'].sampler
     if args.distributed and sampler is not None:
@@ -260,8 +261,8 @@ def main():
             scheduler(step)
 
             if args.synthetic_data:
-                images = torch.randn(args.batch_size, 3, model.visual.image_size,
-                    model.visual.image_size, device=device)
+                images = torch.randn(args.batch_size, 3, model.module.visual.image_size,
+                    model.module.visual.image_size, device=device)
                 texts = tokenize("Test sentence")[0]
                 texts = torch.stack([texts] * args.batch_size)
                 texts = texts.to(device=device, non_blocking=True)

src/training/distributed.py
@@ -77,7 +77,7 @@ def init_distributed_device(args):
         os.environ['RANK'] = str(args.rank)
         os.environ['WORLD_SIZE'] = str(args.world_size)
     elif is_using_distributed():
-        if 'SLURM_PROCID' in os.environ:
+        if 'SLURM_PROCID' in os.environ or 'OMPI_COMM_WORLD_RANK' in os.environ:
             # DDP via SLURM
             args.local_rank, args.rank, args.world_size = world_info_from_env()
             # SLURM var -> torch.distributed vars in case needed