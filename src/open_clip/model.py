b8ba5aba8acbfeae28f800e088c445724f0d1f57
Mehdi Cherti <m.cherti@fz-juelich.de>
2023-01-31 11:48:33 +0100

    resolve conflicts

--- src/open_clip/model.py
+++ src/open_clip/model.py
@@ -227,18 +227,20 @@ class CLIP(nn.Module):
         x = x[torch.arange(x.shape[0]), text.argmax(dim=-1)] @ self.text_projection
         return F.normalize(x, dim=-1) if normalize else x
 
-    def forward(self, image, text):
-        image_features = self.encode_image(image, normalize=True)
-        text_features = self.encode_text(text, normalize=True)
-        with torch.no_grad():
-           self.logit_scale.data.clamp_(0, math.log(100))
+    def forward(self, image, text, clamp_logit_scale_to=None):
+        image_features = self.encode_image(image, normalize=True) if image is not None else None
+        text_features = self.encode_text(text, normalize=True) if text is not None else None
+        if clamp_logit_scale_to is not None:
+            with torch.no_grad():
+                self.logit_scale.data.clamp_(0, clamp_logit_scale_to)
         if self.output_dict:
             return {
                 "image_features": image_features,
                 "text_features": text_features,
                 "logit_scale": self.logit_scale.exp()
             }
-        return image_features, text_features, self.logit_scale.exp()
+        else:
+            return image_features, text_features, self.logit_scale.exp()
 
 
 class CustomTextCLIP(nn.Module):
